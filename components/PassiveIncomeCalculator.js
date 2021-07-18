16d350b9befb6b883654066615a7dd7c2186b333
spikesolution <info@spikesolution.com>
2021-07-18 14:33:21 +0900

    Calculate monthlyIncome on APR/capital change

--- components/PassiveIncomeCalculator.js
+++ components/PassiveIncomeCalculator.js
@@ -1,20 +1,45 @@
 import TextInput from "./TextInput.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function PassiveIncomeCalculator() {
   const [apr, setApr] = useState(0);
   const [capital, setCapital] = useState(0);
   const [income, setIncome] = useState(0);
 
-  const changeCapital = ev => { setCapital(ev.target.value); }
-  const changeIncome = ev => { setIncome(ev.target.value); }
-  const changeApr = ev => { setApr(ev.target.value); }
+  useEffect(() => {
+    console.log("capital: ", capital);
+    console.log("apr: ", apr);
+    console.log("income: ", income);
+
+    const monthlyIncome = (capital * (apr/100)) / 12;
+    console.log("monthlyIncome", monthlyIncome);
+    setIncome(monthlyIncome);
+  });
+
+  const changeCapital = ev => {
+    setCapital(numericValue(ev.target.value));
+  }
+
+  const changeIncome = ev => {
+    setIncome(numericValue(ev.target.value));
+  }
+
+  const changeApr = ev => {
+    setApr(numericValue(ev.target.value));
+  }
+
+  const numericValue = value => {
+    if (value === "") return "";
+
+    const v = parseFloat(value);
+    return isNaN(v) ? 0 : v;
+  }
 
   return (
     <div>
-      <TextInput name="capital" label="Capital ($k)" value={capital} onChange={changeCapital} />
+      <TextInput name="capital" label="Capital ($)" value={capital} onChange={changeCapital} />
       <TextInput name="apr" label="APR (%)" value={apr} onChange={changeApr} />
-      <TextInput name="income" label="Income ($k/month)" value={income} onChange={changeIncome} />
+      <TextInput name="income" label="Income ($/month)" value={income} onChange={changeIncome} />
     </div>
   );
 }
