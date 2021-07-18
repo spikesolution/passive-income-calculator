1ac24b51efa863b9d72d4fa0f4dec2636c7e86c3
spikesolution <info@spikesolution.com>
2021-07-18 15:08:20 +0900

    Calculate capital required for APR & target income

--- components/PassiveIncomeCalculator.js
+++ components/PassiveIncomeCalculator.js
@@ -2,37 +2,64 @@ import TextInput from "./TextInput.js";
 import { useEffect, useState } from "react";
 
 export default function PassiveIncomeCalculator() {
-  const [apr, setApr] = useState(0);
+  const [apr, setApr] = useState(10);
   const [capital, setCapital] = useState(0);
   const [income, setIncome] = useState(0);
-  const [fixed, setFixed] = useState("apr");
+
+  const [fixed, setFixed] = useState("apr"); // Which value is "locked"?
+  const [changed, setChanged] = useState(null); // Which value did the user change?
 
   useEffect(() => {
-    console.log("capital: ", capital);
-    console.log("apr: ", apr);
-    console.log("income: ", income);
-
-    const monthlyIncome = (capital * (apr/100)) / 12;
-    console.log("monthlyIncome", monthlyIncome);
-    setIncome(monthlyIncome);
+    calculateIncome();
+    calculateApr();
+    calculateCapital();
   });
 
+  // What monthly income do we get from a given APR and capital amount?
+  const calculateIncome = () => {
+    if (fixed === "income" || changed === "income") return;
+
+    const monthlyIncome = (capital * (apr/100)) / 12;
+    setIncome(monthlyIncome);
+  }
+
+  // What APR do we need to get the given monthly income from a given capital
+  // amount?
+  const calculateApr = () => {
+    if (fixed === "apr" || changed === "apr") return;
+
+    console.log("calculateApr");
+  }
+
+  // How much capital do we need to get a target monthly income from a given
+  // APR?
+  const calculateCapital = () => {
+    if (fixed === "capital" || changed === "capital") return;
+
+    const yearlyIncome = income * 12;
+    const capitalRequired = yearlyIncome / (apr/100);
+    setCapital(capitalRequired);
+  }
+
   const changeCapital = ev => {
-    if (fixed !== "capital") {
-      setCapital(numericValue(ev.target.value));
-    }
+    if (fixed === "capital") return;
+
+    setCapital(numericValue(ev.target.value));
+    setChanged("capital");
   }
 
   const changeIncome = ev => {
-    if (fixed !== "income") {
-      setIncome(numericValue(ev.target.value));
-    }
+    if (fixed === "income") return;
+
+    setIncome(numericValue(ev.target.value));
+    setChanged("income");
   }
 
   const changeApr = ev => {
-    if (fixed !== "apr") {
-      setApr(numericValue(ev.target.value));
-    }
+    if (fixed === "apr") return;
+
+    setApr(numericValue(ev.target.value));
+    setChanged("apr");
   }
 
   const numericValue = value => {
