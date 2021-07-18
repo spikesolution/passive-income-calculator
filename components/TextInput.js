f41ad83d5b777e174fef8396ab8a14d4562811a2
spikesolution <info@spikesolution.com>
2021-07-18 16:04:12 +0900

    Try to improve layout

--- components/TextInput.js
+++ components/TextInput.js
@@ -5,12 +5,12 @@ export default function TextInput({ name, label, value, onChange, isFixed, butto
   const fixed = isFixed ? "fixed" : "";
   return (
     <div>
-      <label className="text-gray-700">{label}</label>
-      <div className="flex">
-        <div className="w-2/3">
-          <input type="text" id={id} className="w-2/3 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name={name} value={value} onChange={ value => onChange(value) } />
+      <label className="col-span-3 text-gray-700">{label}</label>
+      <div className="grid grid-cols-5">
+        <div className="col-span-3">
+          <input type="text" id={id} className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name={name} value={value} onChange={ value => onChange(value) } />
         </div>
-        <div className="w-1/3">
+        <div className="col-span-2">
           <LockButton locked={isFixed} onClick={buttonClick} />
         </div>
       </div>
