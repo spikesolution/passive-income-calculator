d2c205f5c3234eff203f1376a47205df2b2bca4c
spikesolution <info@spikesolution.com>
2021-08-21 16:28:24 +0900

    UI fixes: full-height buttons w. left margin

--- components/TextInput.js
+++ components/TextInput.js
@@ -10,7 +10,8 @@ export default function TextInput({ name, label, value, onChange, isFixed, butto
         <div className="col-span-3">
           <input type="text" id={id} className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name={name} value={value} onChange={ value => onChange(value) } />
         </div>
-        <div className="col-span-2">
+
+        <div className="flex flex-col justify-between">
           <LockButton locked={isFixed} onClick={buttonClick} />
         </div>
       </div>
